7bd024520391a3000998eeccb1e785cb865d761a
benlei <lei.benjamin@gmail.com>
2025-09-23 19:27:15 -0400

    Update vite.config.js
    change server base

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    }
-  }
+  base: '/app-learning/'
 })
